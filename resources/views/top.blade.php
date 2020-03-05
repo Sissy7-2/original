64929d7ed5a6928b2fd49a6ae7bfc475d55f2322
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-03-05 07:24:20 +0000

    top.blade.phpにparts/headerのinclude出来た★

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,8 +4,9 @@
 @section('content')
 <div class="container">
     <div class="row">
+        @include('parts/header')
         <div class="col-md-8 mx-auto">
-            includeのheader出来ないなんで<br>
+            includeのheaderなんか知らんけど出来た一応頭の中整理しながら<br>
            　目の画像が入る予定
         </div>
     </div>
