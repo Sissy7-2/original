5cdc446e49aa4e6ff078f43d006546a3a8919662
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-03-05 07:10:59 +0000

    webpack.mix.js内容追加

--- public/js/app.js
+++ public/js/app.js
@@ -1170,7 +1170,8 @@ __webpack_require__(12);
 __webpack_require__(43);
 __webpack_require__(44);
 __webpack_require__(45);
-module.exports = __webpack_require__(46);
+__webpack_require__(46);
+module.exports = __webpack_require__(47);
 
 
 /***/ }),
@@ -44887,6 +44888,12 @@ if (false) {
 
 /***/ }),
 /* 46 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 47 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
