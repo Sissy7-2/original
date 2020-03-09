ce3d59c2bbfcfc027208e7956dec2df49c5c69c9
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-03-09 06:26:27 +0000

    migrationファイルrollbackした

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -17,6 +17,33 @@ class MovieController extends Controller
   {
       // admin/news/createにリダイレクトする
       return redirect('admin/movie/create');
+      
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Movie::$rules);
+      
+      $movie = new Movie;
+      $form = $request->all();
+      
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $movie->image_path = basename($path);
+      } else {
+          $movie->image_path = null;
+      }
+      
+       // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $movie->fill($form);
+      $movie->save();
+
+      return redirect('admin/movie/create');
+      
   }  
   
 }
