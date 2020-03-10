1504331773cca72146c0bb7d50eab648f4aef397
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-03-10 13:07:11 +0000

    管理画面うまく行かないusersテーブルまた作った

--- app/Movie.php
+++ app/Movie.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     //
+    protected $table = 'create_movies';
     protected $guarded = array('id');
 
     // 以下を追記
---
+++ database/migrations/2020_03_10_130550_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name'); // ニュースのタイトルを保存するカラム
+            $table->string('answers');  // ニュースの本文を保存するカラム
+            $table->string('image_path')->nullable();  // 画像のパスを保存するカラム
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
