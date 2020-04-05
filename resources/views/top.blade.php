315a86240d60a8812a6f8db71335e7097ec3397b
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-04-05 13:16:27 +0000

    composer.lock Upgrade symfony/http-foundation

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,18 +6,10 @@
         <div class="row">
             @include('parts/header')
             <div class="col-md-12">
-            <div class="mx-auto" style="width:800px;">
             <div class="m">
             <body>
-                    <?php
-                        $image_rand = array(
-                            "img/Righteye.JPG",
-                            "img/Lefteye.JPG",
-                        );
-                         
-                        $image_rand = $image_rand[mt_rand(0, count($image_rand)-1)];
-                        echo '<img src="'.$image_rand.'" alt="">';
-                     ?>
+             <br>
+             <img src="{{ secure_asset('/js/img/Righteye.JPG') }}" class="img-fluid">
             </body>
         </div>
     </div>
