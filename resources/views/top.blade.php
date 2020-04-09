f1f85a68901003be7cd12ecb3a6aa3b45d69bae7
EC2 Default User <ec2-user@ip-172-31-39-223.us-east-2.compute.internal>
2020-04-09 14:28:39 +0000

    topbladephp変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,15 +5,12 @@
     <div class="container">
         <div class="row">
             @include('parts/header')
-            <div class="col-md-12">
-            <div class="m">
             <body>
              <br>
-             <div class="pic">
+             <div class="container">
              <img class="eyespic" src="{{ secure_asset('/js/img/Righteye.JPG') }}">
             </div>
             </body>
-        </div>
     </div>
 </div>
 @endsection
